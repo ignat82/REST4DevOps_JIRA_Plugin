b7fa8fead179b69c7ea11b9f2010bc118a566b36
Marat <gdr075@gmail.com>
2022-06-23 09:39:50 +0400

    webwork

---
+++ src/test/java/ut/HCBplugins/webwork/WebworkActionTest.java
@@ -0,0 +1,34 @@
+package ut.HCBplugins.webwork;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import HCBplugins.webwork.WebWorkAction;
+
+import static org.mockito.Mockito.*;
+
+/**
+ * @since 3.5
+ */
+public class WebworkActionTest {
+
+    @Before
+    public void setup() {
+
+    }
+
+    @After
+    public void tearDown() {
+
+    }
+
+    @Test(expected=Exception.class)
+    public void testSomething() throws Exception {
+
+        //WebworkAction testClass = new WebworkAction();
+
+        throw new Exception("WebworkAction has no tests!");
+
+    }
+
+}
